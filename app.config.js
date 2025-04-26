a14ad28eb828c6e2ec32080479eae0bfc80f8cec
Patrick Heneise <patrick@zentered.co>
2025-04-26 15:52:09 -0600

    fix: app config for cloudflare

--- app.config.js
+++ app.config.js
@@ -1,16 +1,11 @@
 import { defineConfig } from '@solidjs/start/config'
-// import { config } from 'vinxi/plugins/config'
 
 export default defineConfig({
-  // server: {
-  //   compatibilityDate: '2024-11-11',
-  //   preset: 'cloudflare-pages',
-  //   sourceMap: 'inline',
-  //   rollupConfig: {
-  //     external: ['__STATIC_CONTENT_MANIFEST', 'node:async_hooks']
-  //   }
-  //   // prerender: {
-  //   //   crawlLinks: true
-  //   // }
-  // }
+  server: {
+    preset: 'cloudflare-pages',
+
+    rollupConfig: {
+      external: ['node:async_hooks']
+    }
+  }
 })
