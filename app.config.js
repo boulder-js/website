dc07a41812307d0d1267870b52cef0d26ad3d0b4
Patrick Heneise <patrick@zentered.co>
2025-04-26 16:07:26 -0600

    chore: cleanup

--- app.config.js
+++ app.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from '@solidjs/start/config'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
   server: {
     preset: 'cloudflare-pages',
-
     rollupConfig: {
       external: ['node:async_hooks']
     }
