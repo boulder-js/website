58d0f67b28f3b8c59fd518bcd9c6c46f9ad5fcd6
Patrick Heneise <patrick@zentered.co>
2025-10-28 16:54:10 -0600

    fix: bundle gql files

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import solidJs from '@astrojs/solid-js'
 import cloudflare from '@astrojs/cloudflare'
 import tailwindcss from '@tailwindcss/vite'
+import graphqlLoader from 'vite-plugin-graphql-loader'
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,6 +14,13 @@ export default defineConfig({
     solidJs()
   ],
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [
+      tailwindcss(),
+      graphqlLoader()
+    ],
+    ssr: {
+      // Bundle gitevents-fetch so we can handle its .gql file imports
+      noExternal: ['gitevents-fetch', '@zentered/issue-forms-body-parser']
+    }
   }
 })
